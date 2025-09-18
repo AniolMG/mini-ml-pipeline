bdffdde476229f47d4fda6ad05d358be92063045
Aniol Molero <aniolmolerograu@gmail.com>
2025-09-18 12:01:57 +0200

    Added automated tests folder. Run with pytest command.

---
+++ tests/test_api.py
@@ -0,0 +1,8 @@
+import requests
+
+def test_prediction_endpoint():
+    payload = {"Age": 29, "Sex": 1, "Pclass": 3}
+    r = requests.post("http://127.0.0.1:8000/predict", json=payload)
+    assert r.status_code == 200
+    result = r.json()
+    assert "prediction" in result
